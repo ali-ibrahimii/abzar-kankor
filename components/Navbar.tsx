12f96931e0cdc73c1d608e6a3e91ec2637cd258d
Ali Ibrahimi <alimadad.ibrahimi1116@gmail.com>
2025-03-23 04:38:59 +0330

    created flash card page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
             <div className='space-x-12 flex items-center justify-center'>
                 <h1 className='font-bold text-lg'>Logo</h1>
                 <Link className='' href="/">Home</Link>
-                <Link className='' href="/flashcards">flashcards</Link>
+                <Link className='' href="/flashCards">flashcards</Link>
                 <Link className='' href="/library">Library</Link>
                 <Link className='' href="/school">School</Link>
                 <Link className='' href="/ai-learning">Ai Learning</Link>
