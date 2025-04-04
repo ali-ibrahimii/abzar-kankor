6fa28a421b30854eb4f4e3560f7a0f3a422c7a5d
Ali Ibrahimi <alimadad.ibrahimi1116@gmail.com>
2025-04-04 08:58:26 +0330

    create Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,33 +1,78 @@
+
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { Button } from "@/components/ui/button"
 
 
 const Navbar = () => {
+
+    const pathname = usePathname();
+    
   return (
-    <nav className='container px-25 py-5 mx-auto z-10'>
+    <nav className='container relative px-25 py-5 mx-auto z-50'>
         <div className='flex justify-around text-white'>
-            {/* left menu */}
-            <h1 className='font-bold font-[roboto] text-2xl '>Logo</h1>
-            <div className='space-x-12 font-[roboto] font-normal flex items-center justify-center'>
-                <Link className='text-[13px]' href="/">Home</Link>
-                <Link className='text-[13px]' href="/flashCards">Flash Cards</Link>
-                <Link className='text-[13px]' href="/library">Library</Link>
-                <Link className='text-[13px]' href="/school">School</Link>
-                <Link className='text-[13px]' href="/ai-learning">Ai Learning</Link>
-                <Link className='text-[13px]' href="/contact">Contact</Link>
+
+            {/* start menubar */}
+            
+            <div className='space-x-12 font-[roboto] text-5xl flex items-center justify-center'>
+
+                {/* logo */}
+                
+                <Link href="/">
+                    <h1 className='font-bold font-[roboto] text-2xl '>Logo</h1>
+                </Link>
+
+                {/* navigation */}
+                
+                <Link
+                    className={`relative text-[13px] after:content-[''] after:absolute after:left-0 after:-bottom-[5px] after:w-full after:h-[5px] after:rounded-full after:bg-white after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100 ${pathname === '/flashCards' ? 'after:scale-x-100' : ''}`}
+                    href="/">
+                    Home
+                </Link>
+                <Link
+                    className={`relative text-[13px] after:content-[''] after:absolute after:left-0 after:-bottom-[5px] after:w-full after:h-[5px] after:rounded-full after:bg-white after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100 ${pathname === '/flashCards' ? 'after:scale-x-100' : ''}`}
+                    href="/flashCards">
+                    Flash Cards
+                </Link>
+                <Link
+                    className={`relative text-[13px] after:content-[''] after:absolute after:left-0 after:-bottom-[5px] after:w-full after:h-[5px] after:rounded-full after:bg-white after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100 ${pathname === '/flashCards' ? 'after:scale-x-100' : ''}`}
+                    href="/ai-learning">
+                    Ai Learning
+                </Link>
+                <Link
+                    className={`relative text-[13px] after:content-[''] after:absolute after:left-0 after:-bottom-[5px] after:w-full after:h-[5px] after:rounded-full after:bg-white after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100 ${pathname === '/flashCards' ? 'after:scale-x-100' : ''}`}
+                    href="/library">
+                    Library
+                </Link>
+                <Link
+                    className={`relative text-[13px] after:content-[''] after:absolute after:left-0 after:-bottom-[5px] after:w-full after:h-[5px] after:rounded-full after:bg-white after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100 ${pathname === '/flashCards' ? 'after:scale-x-100' : ''}`}
+                    href="/contact">
+                    Contact
+                </Link>
+                
             </div>
 
-            {/* right menu */}
+            {/* end menubar */}
+
+
+            {/* start navbar btn */}
             
             <div className='flex flex-row items-center space-x-2'>
-                <Button className='font-[roboto] rounded-full bg-white text-black hover:bg-transparent hover:border-2 font-normal cursor-pointer' asChild>
-                    <Link className='cursor-pointer' href="/login">Login</Link>
+                {/* login btn */}
+                <Button className='cursor-pointer font-[roboto] rounded-full bg-white text-black hover:bg-transparent hover:border-2 border-2 hover:text-white font-normal transition-all duration-300' asChild>
+                    <Link className='' href="/login">Login</Link>
                 </Button>
-                <Button className='rounded-full font-[roboto] bg-transparent border-2 text-white hover:bg-black font-normal cursor-pointer' asChild>
-                    <Link className='cursor-pointer' href="/login">Register</Link>
+                {/* register btn */}
+                <Button className='cursor-pointer rounded-full font-[roboto] bg-transparent border-2 text-white hover:bg-white hover:border-2 hover:text-black font-normal transition-all duration-300' asChild>
+                    <Link className='' href="/login">Register</Link>
                 </Button>
             </div>
+
+            {/* end navbar btn */}
+
         </div>
     </nav>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,21 @@ import { Button } from "@/components/ui/button"
 
 const page = () => {
   return (
-    <div className="p-50 relative w-full h-screen overflow-hidden">
+    <div className="relative w-full h-screen overflow-hidden">
+
+      <video
+      loop muted autoPlay playsInline
+      className="absolute left-0 top-0 w-full h-screen object-cover "
+      src="background.mp4"></video>
+
 
         {/* start main page  */}
         
       <div className="px-32">
-        <video 
-          src="/background.mp4" 
-          className="absolute top-0 left-0 w-full h-full object-cover" 
-          autoPlay loop muted 
-        />
-        <div className="absolute bottom-0 left-0 w-full h- bg-gradient-to-t from-black to-transparent z-20" />
-        <div className="absolute top-0 left-0 w-full z-10">
+        <div className="absolute top-0 left-0 w-full z-50">
           <Navbar />
         </div>
+        <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black to-transparent z-20" />
         <div className="absolute top-0 left-0 px-38 z-10 flex flex-col items-start justify-center h-full text-white">
           <h1 className="text-5xl font-bold font-[roboto]">Abzar Kankor</h1>
           <p className="mt-4 text-md w-5/12 font-[roboto] text-[13px]">
@@ -29,12 +30,16 @@ const page = () => {
           </p>
         </div>
         <div className="absolute top-0 left-0 w-full h-full bg-black/50 z-0"></div>
-        <Button className='absolute bottom-2 font-[roboto] rounded-full bg-white text-black hover:bg-transparent hover:border-2 font-normal cursor-pointer' asChild>
+        <Button className='absolute bottom-2 font-[roboto] rounded-full bg-white text-black font-normal z-50 hover:bg-gray-200' asChild>
           <Link className='cursor-pointer' href="/flashcard">Start Learning</Link>
         </Button>
       </div>
 
           {/* end main page  */}
+
+          <div>
+
+          </div>
           
     </div>
   );
